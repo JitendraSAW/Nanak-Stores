afab1e9a3a9cf7cc31625f765a311c5df112d8dd
tailorraj <tailorraj111@gmail.com>
2023-11-08 11:22:03 +0000

    field name changes

--- nanak_customization/nanak_customization/doctype/nanak_standard_settings/nanak_standard_settings.py
+++ nanak_customization/nanak_customization/doctype/nanak_standard_settings/nanak_standard_settings.py
@@ -1,8 +1,25 @@
 # Copyright (c) 2022, Raaj Tailor and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class NanakStandardSettings(Document):
 	pass
+
+
+@frappe.whitelist()
+def update_custom_picked_qty():
+	# Update custom_picked_qty for records where picked_qty > 0
+	
+	query = """
+	UPDATE `tabSales Order Item` soi
+	SET soi.custom_picked_qty = soi.picked_qty
+	where
+	soi.picked_qty > 0;
+	"""
+	frappe.db.sql(query)
+	frappe.db.commit()
+
+	frappe.msgprint("custom_picked_qty has been updated for records where picked_qty > 0.")
+
