6aedcf9269cb9775ff0eda1750eb43aa2f77a7f4
tailorraj <tailorraj111@gmail.com>
2024-10-28 15:51:31 +0500

    bug fix for delete draft state sales invoice

--- nanak_customization/nanak_customization/sales_invoice.py
+++ nanak_customization/nanak_customization/sales_invoice.py
@@ -8,6 +8,31 @@ def validate(self,method):
     if self.picklist_reference:
         frappe.db.set_value("Nanak Pick List", self.picklist_reference, "sales_invoice", self.name)
 
+def on_trash(self, method):
+    # frappe.throw("on trash event")
+    try:
+        if self.picklist_reference and self.docstatus == 0:
+            frappe.db.set_value("Sales Invoice", self.name, {
+                "picklist_reference": ""
+            })
+            frappe.db.set_value("Nanak Pick List", self.picklist_reference, {
+                "sales_invoice": "",
+                "sales_invoice_status": ""
+            })
+
+            frappe.db.sql("""
+                UPDATE `tabSales Invoice Item`
+                SET nanak_pick_list = '', pick_list_details = ''
+                WHERE parent = %s
+            """, (self.name,))
+            
+            frappe.db.commit()
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(),"on trash event error")
+
+        
+
+
 @frappe.whitelist()
 def unlink_vouchers(nanak_pick_list, sales_invoice):
     try:
